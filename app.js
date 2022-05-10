c716409469ed36ad0e388f7087d8000372bd8e25
Reon Ji <jcg@inka.co.kr>
2022-05-10 15:40:31 +0900

    change app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.set('views'.'./views');
-app.set('view engine'.'ejs');
+app.set('views','./views');
+app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
   res.render('shaka');
